b74f5f9f9b98ddb2fdf18a756f8f335b14d2a1b4
Pablo Garnham <pgarnham@uc.cl>
2021-06-05 18:35:23 -0400

    vuetify y router funcionando

---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+/* import { createRouter, createWebHashHistory } from 'vue-router'
+import App from '../App.vue'
+import Detail from '../components/Details.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'App',
+    component: App
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: Detail
+  }, {
+    path: '/detail',
+    name: 'Detail',
+    component: Detail
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router */
+
+import App from '../App.vue'
+import Detail from '../components/Details.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+export default new Router({
+    mode: 'history',
+    routes: [
+      //Download page
+      {
+        path: '/',
+        name: 'App',
+        component: App
+      },
+      {
+        path: '/detail',
+        name: 'Detail',
+        component: Detail
+      }, {
+        path: '/detail',
+        name: 'Detail',
+        component: Detail
+      },
+    ]
+  })
